6d3b6347e1bb59a31eec075b50dd9e8a5781049f
john.dev62 <john.dev62@outlook.fr>
2026-07-22 14:22:55 +0200

    Passe en v1.1.16

--- gui.py
+++ gui.py
@@ -20,7 +20,7 @@ from tkinter import ttk, messagebox, filedialog, simpledialog
 from PIL import Image, ImageDraw, ImageTk
 
 DONATE_URL = "https://ko-fi.com/yoshines62000"
-APP_VERSION = "1.1.15"
+APP_VERSION = "1.1.16"
 UPDATE_REPO = "yoshines62000-alt/GuideExpress"
 RELEASES_URL = f"https://github.com/{UPDATE_REPO}/releases/latest"
 
